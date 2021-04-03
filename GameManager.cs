9b787158a73b31f68d49345ca32c0d63947f96b7
debbie-tan <81861011+debbie-tan@users.noreply.github.com>
2021-04-03 19:36:44 +0200

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    int score;
+
+    public Text scoreText;
+    public GameObject gameStartUI;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GameStart()
+    {
+        gameStartUI.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
